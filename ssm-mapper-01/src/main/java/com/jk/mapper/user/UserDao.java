bae2602f0c61b21328e4dbdf7b5de4a717cce176
Richard <git@github.com:yuejr/yuejr.git>
2017-05-23 20:28:30 +0800

    提交

---
+++ ssm-mapper-01/src/main/java/com/jk/mapper/user/UserDao.java
@@ -0,0 +1,7 @@
+package com.jk.mapper.user;
+
+/**
+ * Created by Richard on 2017/5/23.
+ */
+public class UserDao {
+}
---
+++ ssm-service-01/src/main/java/com/jk/service/user/UserServiceImpl.java
@@ -0,0 +1,10 @@
+package com.jk.service.user;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Richard on 2017/5/23.
+ */
+@Service
+public class UserServiceImpl implements UserService {
+}
